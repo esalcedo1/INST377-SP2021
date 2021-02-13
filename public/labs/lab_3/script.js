52cb68c751420535745c2342a8f4e57b4039cb3b
Ethan Salcedo <53275250+esalcedo1@users.noreply.github.com>
2021-02-12 22:39:13 -0500

    final submission
    Changed the for loop to an array.foreach

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,14 +1,39 @@
-/* Put your javascript in here */
-"use strict";
+/* label the images, just for convenience, to visually track them */
+const images = document.querySelectorAll('.imageClass');
+const imageArray = Array.from(images);
 
-let width = 200;
-let count = 7;
-let currentCount = 1;
+let i = 1;
 
-function shiftRight(){
-   alert('would shift right with some good code');
+function assign(item) {
+    item.style.position = 'relative';
+    item.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`)
+    i++;
 }
 
-function shiftLeft(){    
-    alert("would shift left with some even better code");
+imageArray.forEach(assign);
+
+/* configuration */
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
 }
+
+carousel.querySelector('.next').onclick = function() {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+}
+
