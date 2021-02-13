3de2bfe6a91dfdb552313a8092340fb2b2ba1eae
Ethan Salcedo <53275250+esalcedo1@users.noreply.github.com>
2021-02-12 19:42:11 -0500

    Added a lot
    html and CSS structure are done. Linked the JS script to the HTML file and am stuck on creating the functions to shift the carousel.

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,2 +1,14 @@
 /* Put your javascript in here */
 "use strict";
+
+let width = 200;
+let count = 7;
+let currentCount = 1;
+
+function shiftRight(){
+   alert('would shift right with some good code');
+}
+
+function shiftLeft(){    
+    alert("would shift left with some even better code");
+}
