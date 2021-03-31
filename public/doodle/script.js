5415cad2d6cd3cd5e4c1d1345e310fa42fe1e11e
Ethan Salcedo <53275250+esalcedo1@users.noreply.github.com>
2021-03-31 11:09:05 -0400

    Doodle Start
    start of the doodle jump

---
+++ public/doodle/script.js
@@ -0,0 +1,6 @@
+/// it's either this at the top of your script.js files or having your script tag at the end of your HTML file
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const doodler = document.createElement('div');
+
+})
